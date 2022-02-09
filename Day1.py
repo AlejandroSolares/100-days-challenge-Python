f1ae99ea3328686d119e4d29bcdf34a82dbe7f5e
Alejandro Solares <josue.alejandro.solares@gmail.com>
2022-02-09 17:02:51 -0600

    day 1

---
+++ Day1.py
@@ -0,0 +1,15 @@
+print("Band Name Generator!")
+
+#User name input
+uname = input("What's your name or nickname?\n")
+
+#City or Street name input
+street = input("What's name of the city you grew up in?\n")
+
+#Pet name
+pet = input("What's your pet's name?\n")
+
+
+print("Your band name could be: \n  The " + street + " " + pet + "'s \n")
+print("  " + uname + " and the " + pet + "'s \n")
+print("  " + uname + "'s " + street + " sensational band \n")
